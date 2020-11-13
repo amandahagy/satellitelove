aec32dc5fa956b1fad046da64bbe2a9a0ed994a4
Amanda Hagy <amandahagy@hotmail.com>
2020-11-13 06:20:14 -0300

    UserBean

---
+++ src/com/shiningstars/satellitelove/bean/UserBean.java
@@ -0,0 +1,47 @@
+package com.shiningstars.satellitelove.bean;
+
+import java.util.Locale;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import com.shiningstars.satellitelove.entity.User;
+
+@ManagedBean
+public class UserBean {
+	private User user;
+	
+	@PostConstruct
+	private void init() {
+		user = new User();
+	}
+	
+	public void create() {
+		System.out.println(user.getEmail());
+		System.out.println(user.getPassword());
+		System.out.println(user.getBirthday());
+		System.out.println(user.getCreationDate());
+		System.out.println(user.getSun());
+		System.out.println(user.getAsc());
+		System.out.println(user.getMoon());
+		System.out.println(user.getVenus());
+		System.out.println(user.getMars());
+		System.out.println(user.getEmail());
+	
+	FacesMessage msg = new FacesMessage("User successfully created!");
+	FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+	
+	//Post homepage
+	public String navegateHome() {
+		return "index?faces-redirect=true";
+	}
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
