da6ff4f3c39aff6b61c1be8eb013a5649512741b
Amanda Hagy <amandahagy@hotmail.com>
2020-11-13 06:32:29 -0300

    LanguageBean

---
+++ src/com/shiningstars/satellitelove/bean/LanguageBean.java
@@ -0,0 +1,45 @@
+package com.shiningstars.satellitelove.bean;
+
+import java.io.Serializable;
+import java.util.Locale;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LanguageBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private Locale locale;
+	
+	@PostConstruct
+	private void init(){
+		locale = FacesContext.getCurrentInstance().getViewRoot()
+			.getLocale();
+	}
+
+	public void changeToEnglish() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		UIViewRoot view = context.getViewRoot();
+		locale = Locale.ENGLISH;
+		view.setLocale(locale);
+	}
+
+	public void changeToPortuguese() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		UIViewRoot view = context.getViewRoot();
+		locale = new Locale("pt", "BR");
+		view.setLocale(locale);
+	}
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+}
